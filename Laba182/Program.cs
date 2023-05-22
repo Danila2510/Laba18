cc1ad5612b6e78458493fc5f2a737a74218d737d
Danila <Danila@DESKTOP-B319KEU>
2023-05-22 15:16:02 +0300

    Добавьте файлы проекта.

---
+++ Laba182/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab18
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> numbers = new List<string>() { "Bi", " Ba", " i ", "Bo", "Ba " };
+            Thread thread = new Thread(() =>
+            {
+                foreach (string number in numbers)
+                    Console.Write($" {number}");
+            });
+            thread.Start();
+            thread.Join();
+            Console.ReadLine();
+        }
+    }
+}
